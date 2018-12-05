9bc286d3aa611ae2f979872cebd5ee987c91f119
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-05 16:02:20 +0200

    Creating ordered list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', null, "Hello world!");
+const element = React.createElement('ol', null,
+    React.createElement('li', null, 'Take out the trash'),
+    React.createElement('li', null, 'shovel the driveway'),
+    React.createElement('li', null, 'walk the dog')
+    );
+
+ReactDOM.render(element, document.getElementById('root'));
