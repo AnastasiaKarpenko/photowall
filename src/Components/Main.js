45c57aa5746a4af3d33041be677b051196fcd904
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-06 21:31:57 +0200

    Adding componentDidUpdate method for showing the alert message

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,7 +6,21 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            posts: []
+            posts: [{
+                id: "0",
+                description: "beautiful landscape",
+                imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
+            }, {
+                id: "1",
+                description: "Aliens?",
+                imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
+
+            }, {
+                id: "2",
+                description: "On a vacation",
+                imageLink: "https://welldesi.files.wordpress.com/2015/10/hanauma-bay.jpg?w=2400&h=1600&crop=1"
+
+            }]
         }
         this.removePhoto = this.removePhoto.bind(this);
         console.log('constructor')
@@ -21,10 +35,6 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        const data = SimulateFetchFromDatabase();
-        this.setState({
-            posts: data
-        })
         console.log('componentDidMount')
     }
 
@@ -41,22 +51,5 @@ class Main extends Component {
     }
 }
 
-function SimulateFetchFromDatabase() {
-    return [{
-        id: "0",
-        description: "beautiful landscape",
-        imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
-    }, {
-        id: "1",
-        description: "Aliens?",
-        imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
-
-    }, {
-        id: "2",
-        description: "On a vacation",
-        imageLink: "https://welldesi.files.wordpress.com/2015/10/hanauma-bay.jpg?w=2400&h=1600&crop=1"
-
-    }]
-}
 
 export default Main
