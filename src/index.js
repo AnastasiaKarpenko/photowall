c2185d5e36ec0ba1e4e1d970d421d1914b531ed2
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-06 08:26:54 +0200

    Wiring the key to the index of the array

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
 const element = <ol>
-    {tasks.map((task) => <li key = {task}>{task}</li>)}
+    {tasks.map((task, index) => <li key = {index}>{task}</li>)}
 </ol>
 
 ReactDOM.render(element, document.getElementById('root'));
