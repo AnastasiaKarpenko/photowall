54137509072336c6e4aa7cd12bfdef3e33616c64
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-06 17:43:33 +0200

    Adding keys to photos in the photowall, returning the figure in the Photo render method.

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -4,8 +4,8 @@ import Photo from './Photo'
 class PhotoWall extends Component {
     render() {
         return <div>
-            {this.props.posts.map(post => <Photo post = {post}/>)}
-            </div>
+            {this.props.posts.map((post, index) => <Photo key={index} post = {post}/>)}
+        </div>
     }
 }
 
