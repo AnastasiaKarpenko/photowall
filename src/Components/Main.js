f6778c171d9bbd3f404d14d8cdf1064d84d2fa0d
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-06 16:45:29 +0200

    Importing the new font, styling the text, Creating a new PhotoWall class

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react'
 import Title from './Title'
+import PhotoWall from './PhotoWall'
 
+const posts = [{
+    id: "0",
+    description: "beautiful landscape",
+    imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
+}, { id: "1",
+    description: "Aliens?",
+    imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
+
+}, { id: "2",
+    description: "On a vacation",
+    imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
+
+}]
 
 class Main extends Component {
     render() {
