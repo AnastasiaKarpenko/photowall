788b991eda2b216a9da9c1a9e99b664369663ad5
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-06 18:09:35 +0200

    Adding the right links of the photos, adding source of the photos and description to the photo properties

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,11 +8,11 @@ const posts = [{
     imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
 }, { id: "1",
     description: "Aliens?",
-    imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
+    imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
 
 }, { id: "2",
     description: "On a vacation",
-    imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
+    imageLink: "https://welldesi.files.wordpress.com/2015/10/hanauma-bay.jpg?w=2400&h=1600&crop=1"
 
 }]
 
