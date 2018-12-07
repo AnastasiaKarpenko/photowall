c7ff83857101bbcd03227d984f7df953095ac46b
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-07 16:51:46 +0200

    making id of each photo a number, new photo will have a date as an id

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,16 +9,16 @@ class Main extends Component {
         super()
         this.state = {
             posts: [{
-                id: "0",
+                id: 0,
                 description: "beautiful landscape",
                 imageLink: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg"
             }, {
-                id: "1",
+                id: 1,
                 description: "Aliens?",
                 imageLink: "https://static1.squarespace.com/static/53a7cd8fe4b09956480529e3/53a875e5e4b0ff78ff7bb855/53a8760ce4b0ff78ff7bbfb6/1403549337779/20100716_Yosemite_449_16x24.jpg?format=2500w"
 
             }, {
-                id: "2",
+                id: 2,
                 description: "On a vacation",
                 imageLink: "https://welldesi.files.wordpress.com/2015/10/hanauma-bay.jpg?w=2400&h=1600&crop=1"
 
