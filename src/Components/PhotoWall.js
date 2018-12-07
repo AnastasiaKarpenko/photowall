eeaa710c06c80be4b5fcba0960074d382ffd8206
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-07 14:51:39 +0200

    Added a Link component to the button

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Photo from './Photo'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
-//anchor tag, href attribute
 function PhotoWall(props) {
     return <div>
-        <a onClick={props.onNavigate} className="addIcon" href="#AddPhoto"> </a>
+        <Link className="addIcon" to="/AddPhoto"> </Link>
         <div className="photo-grid">
             {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
         </div>
