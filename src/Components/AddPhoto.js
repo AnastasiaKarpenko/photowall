1814a1b7ecb1ee959c6f77c0767312a643e630b8
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-07 16:01:05 +0200

    Created the method handleSubmit and added to the property of the form tag onSubmit

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react'
 
 class AddPhoto extends Component {
+
+    handleSubmit() {
+
+    }
+
+
     render() {
         return (
             <div>
                 <h1> Photowall </h1>
-                <form className="form">
-                    <input type="text" placeholder="Link"/>
-                    <input type="text" placeholder="Description"/>
-                    <button className="button">Post</button>
-                </form>
+                <div className="form">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Link"/>
+                        <input type="text" placeholder="Description"/>
+                        <button className="button">Post</button>
+                    </form>
+                </div>
+
             </div>
         )
     }
