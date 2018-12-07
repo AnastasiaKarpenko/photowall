764b96a23fcaad3cb697585c4e3c8d24f1dc7647
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-07 10:42:57 +0200

    Added the button and styles for the button

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -3,8 +3,11 @@ import Photo from './Photo'
 import PropTypes from 'prop-types'
 
 function PhotoWall(props) {
-    return <div className="photo-grid">
-        {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+    return <div>
+        <button className="addIcon"> + </button>
+        <div className="photo-grid">
+            {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+        </div>
     </div>
 }
 
